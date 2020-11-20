d3154578602600a7287d1d4bf55bcf901c1612c9
Noah <noah.buchs@outlook.com>
2020-11-20 14:02:01 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Blog</title>
+</head>
+<body>
+    <header>
+    <h1> The Blogger</h1>
+    <p>Stories for everyone</p>
+    
+    </header>
+<nav>
+<div class="navbar">
+<ul>
+  <li><a href="">Home</a></li>
+  <li><a href="">Profile</a></li>
+  <li><a href="latest.php">Latest Blogs</a></li>
+  <div class="others">
+  <li><a href="">News</a></li>
+  <li><a href="">About us</a></li>
+  <li><a href="">Help</a></li>
+  <li><a href="">Settings</a></li>
+</div>
+</ul>
+</div>
+</nav>
+<main> 
+<h1>Write a comment</h1>
+
+<form action="" method="post">
+<label>Name</label><br>
+<input type="text" name="name">
+<br>
+<label>Comment</label><br>
+<textarea rows="8" cols="22" name="comment">    
+</textarea>
+<br>
+<label>Reaction</label><br>
+<textarea name="reaction" cols="30" rows="1"></textarea><br>
+</label>
+<input type="submit" name="submit"   value="Submit">
+
+
+
+
+</main>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ latest.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latest Blogs</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <header>
+    <h1>Latest Blogs</h1>
+    </header>
+
+    <nav>
+<div class="navbar">
+<ul>
+  <li><a href="">Home</a></li>
+  <li><a href="">Profile</a></li>
+  <li><a href="latest.php">Latest Blogs</a></li>
+  <div class="others">
+  <li><a href="">News</a></li>
+  <li><a href="">About us</a></li>
+  <li><a href="">Help</a></li>
+  <li><a href="">Settings</a></li>
+</div>
+</ul>
+</div>
+</nav>
+</body>
+</html>
