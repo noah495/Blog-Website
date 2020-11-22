f4ab35c9a4c6e313734db5979d0bbd26230e07c3
Noah <noah.buchs@outlook.com>
2020-11-22 11:21:04 +0100

    commit 2

--- latest.php
+++ latest.php
@@ -15,7 +15,7 @@
     <nav>
 <div class="navbar">
 <ul>
-  <li><a href="">Home</a></li>
+  <li><a href="index.php">Home</a></li>
   <li><a href="">Profile</a></li>
   <li><a href="latest.php">Latest Blogs</a></li>
   <div class="others">
@@ -27,5 +27,21 @@
 </ul>
 </div>
 </nav>
+<main>
+    <div class="blog-a">
+        <div class="text-a">
+        <h2>Blog by ...</h2>
+        <h3>Underwater President</h3>
+        <p> The ice is melting, the world is warming, and the oceans are rising. Richer countries bear most of the responsibility for 
+            global warming, but poorer island countries like the Maldives are the first to bear the brunt of rising waters.
+            The Maldives are a chain of islands famous … </p>
+        </div>
+            <img class="pic-a" src="static/img/sea.jpg" alt="sea" height= 400px width= 460px>
+    </div>
+    <div class="blog-b">
+
+
+    </div>
+</main>
 </body>
 </html>
